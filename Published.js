a15a7399d034cf4cb538b9fc695a68da01468da5
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-06-30 22:59:05 +0530

    g

--- Published.js
+++ Published.js
@@ -6,8 +6,8 @@ const mqttBrokerUrl = 'mqtt://test.mosquitto.org:1883';
 // Create an MQTT client
 const mqttClient = mqtt.connect(mqttBrokerUrl);
 //added when it pushed docker
-const data1 = require('/app1/MQTT-PUBLISHER/alaram/agentsTSsub1.json')
-const data2 = require('/app1/MQTT-PUBLISHER/alaram/TS.json')
+const data1 = require('/app11/MQTT-PUBLISHER/alaram/agentsTSsub1.json')
+const data2 = require('/app11/MQTT-PUBLISHER/alaram/TS.json')
 fs.readFile(data1, data2, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
