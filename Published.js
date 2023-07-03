2db9fced21e9db2015d5406f3cb43321f18139ac
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-07-03 14:32:54 +0530

    changes

--- Published.js
+++ Published.js
@@ -6,7 +6,7 @@ const password = '26afc6e1';
 const clientId = '95cb7981-3d4c-4b35-aff0-73d5150be1be';
 const host = '10.246.0.10:1883';
 let messageCount = 0;
-//const topic = 'MC/V1/testing';
+const topic = 'MC/V1/testing';
 const MQTT_QOS = 1;
 // Relative file paths within the container
 const relativeFilePath1 = './alaram/agentsTSsub1.json';
@@ -25,21 +25,20 @@ const client1 = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
     rejectUnauthorized: false
 });
 
-setInterval(() => {
-    client.on('connect', () => {
-        topicFileMappings.forEach(mapping => {
-            const { topic, file } = mapping;
-            fs.readFile(file, 'utf8', (err, data) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
 
-                // Publish the file contents to the corresponding topic
-                client.publish(topic, data);
+client.on('connect', () => {
+    topicFileMappings.forEach(mapping => {
+        const { topic, file } = mapping;
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
 
-                console.log(`Published file contents to topic ${topic}`);
-            });
+            // Publish the file contents to the corresponding topic
+            client.publish(topic, data);
+
+            console.log(`Published file contents to topic ${topic}`);
         });
     });
-}, 6000);
+});
