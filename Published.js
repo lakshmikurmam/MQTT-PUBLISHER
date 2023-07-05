0b210e5da5c3192048afcbf4009e05b7399d0e36
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-07-05 17:16:11 +0530

    changes pushed 7/5

--- Published.js
+++ Published.js
@@ -10,16 +10,16 @@ const topic = 'MC/V1/testing';
 const MQTT_QOS = 1;
 // Relative file paths within the container
 const relativeFilePath1 = './alaram/agentsTSsub1.json';
-const relativeFilePath2 = './alaram/TS.json';
+//const relativeFilePath2 = './alaram/TS.json';
 const absoluteFilePath1 = path.join(__dirname, relativeFilePath1);
 const absoluteFilePath2 = path.join(__dirname, relativeFilePath2);
 const topicFileMappings = [
     { topic: 'MC/V1/testing', file: absoluteFilePath1 },
     // { topic: 'testing_new', file: absoluteFilePath2 },
-    { topic: 'MC/V1/testing', file: absoluteFilePath2 },
+    // { topic: 'MC/V1/testing', file: absoluteFilePath2 },
 ];
 //Local Test
-//const client = mqtt.connect('mqtt://test.mosquitto.org:1883'); // Replace with your MQTT broker URL
+//const client1 = mqtt.connect('mqtt://test.mosquitto.org:1883'); // Replace with your MQTT broker URL
 const client = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
     clientId,
     clean: true,
@@ -40,6 +40,11 @@ client.on('connect', () => {
             client.publish(topic, data);
 
             console.log(`Published file contents to topic ${topic}`);
+            client.end(); // Disconnect from the MQTT broker after publishing
+
         });
+
     });
+
+
 });
