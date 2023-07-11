e32c0b5256fe0eedb7be843d50cf77607660b26d
Gudimetla <503247350@ge.com>
2023-07-11 21:30:05 +0530

    changes pushed

--- Published.js
+++ Published.js
@@ -12,14 +12,14 @@ const MQTT_QOS = 1;
 const relativeFilePath1 = './alaram/agentsTSsub1.json';
 //const relativeFilePath2 = './alaram/TS.json';
 const absoluteFilePath1 = path.join(__dirname, relativeFilePath1);
-const absoluteFilePath2 = path.join(__dirname, relativeFilePath2);
+
 const topicFileMappings = [
     { topic: 'MC/V1/testing', file: absoluteFilePath1 },
     // { topic: 'testing_new', file: absoluteFilePath2 },
     // { topic: 'MC/V1/testing', file: absoluteFilePath2 },
 ];
 //Local Test
-//const client1 = mqtt.connect('mqtt://test.mosquitto.org:1883'); // Replace with your MQTT broker URL
+const client1 = mqtt.connect('mqtt://test.mosquitto.org:1883'); // Replace with your MQTT broker URL
 const client = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
     clientId,
     clean: true,
